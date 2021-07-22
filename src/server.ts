f0cbef68b6e137490d3ebb65af2db4a5f673e363
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-22 17:38:08 -0300

    rotas

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,16 @@ import express from 'express';
 
 const app = express();
 
+app.get('/test', (request, response) => {
+    return(
+        response.send('ola nlw')
+    )
+})
+
+app.post('/test-post', (request, response) => {
+    return(
+        response.send('ola nlw metodo post')
+    )
+})
+
 app.listen(3000, () => console.log('server running 💙'))
