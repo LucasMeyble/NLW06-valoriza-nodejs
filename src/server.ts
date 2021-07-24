b7ccf345ef721ddf6f570d449a6fe687249a932f
lucas Meyble <lucasmeyble123@gmail.com>
2021-07-23 21:09:25 -0300

    feat: configurando migrations com typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,7 @@
+import "reflect-metadata"
+import "./database"
 import express from 'express';
 
 const app = express();
-
-app.get('/test', (request, response) => {
-    return(
-        response.send('ola nlw')
-    )
-})
-
-app.post('/test-post', (request, response) => {
-    return(
-        response.send('ola nlw metodo post')
-    )
-})
 
 app.listen(3000, () => console.log('server running 💙'))
