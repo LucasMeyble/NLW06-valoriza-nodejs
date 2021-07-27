920dc1c731d3c525d26861795dac45d0314454cf
Lucas Meyble <lucasmeyble123@gmail.com>
2021-07-27 11:35:16 -0300

    feat: tratando exceções e conhecendo um pouco de middlewares

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,26 @@
 import "reflect-metadata"
 import "./database"
+import "express-async-errors"; //tem q ser importado aqui, nao sei pq
 import { router } from "./routes";
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 
 const app = express();
 
 app.use(express.json())
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "internal Server Error"
+    })
+})
 
 app.listen(3000, () => console.log('server running 💙')) 
